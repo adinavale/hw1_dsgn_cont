5830fff0517a3cae21d93d3ee84531af0512300a
adinavale <68146367+adinavale@users.noreply.github.com>
2022-09-24 15:52:07 -0700

    Updated inputs and outputs

--- files/vid5a.sv
+++ files/vid5a.sv
@@ -4,29 +4,27 @@ module vid5a(
     input clk,
     input reset,
 
-    input selin,
-    input [2:0] cmdin,
-    input [1:0] lenin, //
+    output selin,
+    output [2:0] cmdin,
+    output [1:0] lenin, 
     output [31:0] addrdatain,
 
-    output [1:0] reqout,
-    output [1:0] lenout,
-    output [31:0] addrdataout,
-    output [2:0] cmdout,
+    input [1:0] reqout,
+    input [1:0] lenout,
+    input [31:0] addrdataout,
+    input [2:0] cmdout,
     output [3:0] reqtar,
 
-    input ackin,
-    input enable, //
+    output ackin,
+    output enable, //
     output hsync, //
-    input hblank, //
-    input vsync,
-    input vblank,
+    output hblank, //
+    output vsync,
+    output vblank,
 
-    output [7:0] R,
-    output [7:0] G,
-    output [7:0] B
+    input [7:0] R,
+    input [7:0] G,
+    input [7:0] B
     );
 
-    assign addrdatain = 5;
-    assign hsync = 1;
 endmodule
