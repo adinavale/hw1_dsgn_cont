bb73650dcfc4400bc3a22b56b1bec3f60161ecba
adinavale <68146367+adinavale@users.noreply.github.com>
2022-09-24 17:22:06 -0700

    input/output fix

--- files/vid5a.sv
+++ files/vid5a.sv
@@ -4,27 +4,32 @@ module vid5a(
     input clk,
     input reset,
 
-    output selin,
-    output [2:0] cmdin,
-    output [1:0] lenin, 
-    output [31:0] addrdatain,
+    input selin,
+    input [2:0] cmdin,
+    input [1:0] lenin, 
+    input [31:0] addrdatain,
 
-    input [1:0] reqout,
-    input [1:0] lenout,
-    input [31:0] addrdataout,
-    input [2:0] cmdout,
+    output [1:0] reqout,
+    output [1:0] lenout,
+    output [31:0] addrdataout,
+    output [2:0] cmdout,
     output [3:0] reqtar,
-
     output ackin,
+
     output enable, //
     output hsync, //
     output hblank, //
     output vsync,
     output vblank,
 
-    input [7:0] R,
-    input [7:0] G,
-    input [7:0] B
+    output [7:0] R,
+    output [7:0] G,
+    output [7:0] B
     );
 
+    always_ff @( posedge clk ) begin : blockName
+        if (reset) begin
+            ackin = 
+        end
+    end
 endmodule
