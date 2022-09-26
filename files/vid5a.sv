0f96d380acc32a2ee9604df27557106aa3b1e2a1
adinavale <68146367+adinavale@users.noreply.github.com>
2022-09-26 15:55:42 -0700

    Changed to burst of 8

--- files/vid5a.sv
+++ files/vid5a.sv
@@ -167,7 +167,7 @@ module vid5a(
     else if (ackin && en && (cmdin == 3'b000) ) begin
         cmdout = 3'b010; //Makes read request from tb
         addrdataout = base_address + lineinc*2;
-        lenout = 2'b11; //Makes 4 transfers for a request
+        lenout = 2'b10; //Makes 4 transfers for a request
         reqtar = 4'b0000; //Targets memory system
         
         /*if (cmdin == 3'b000) begin
