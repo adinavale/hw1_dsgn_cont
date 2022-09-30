1752dafd923d19a59aa14faffd411291e9730a48
adinavale <68146367+adinavale@users.noreply.github.com>
2022-09-30 00:13:26 -0700

    Fixing base address offset for writing registers

--- files/vid5a.sv
+++ files/vid5a.sv
@@ -108,26 +108,26 @@ always @ (*) begin
                 cr_reg.pcnt = addrdatain[9:4];
                 cr_reg.vclk = addrdatain[15:14];
                 prog_st_d = wr_req;
-            end else if (addrdatain_d == 28) begin
+            end else if (addrdatain_d == 32'h00000028) begin
                 h1_reg.hend = addrdatain[12:0];
                 h1_reg.hsize = addrdatain[25:13];
                 prog_st_d = wr_req;
-            end else if (addrdatain_d == 30) begin
+            end else if (addrdatain_d == 32'h00000030) begin
                 h2_reg.hsync_start = addrdatain[25:13];
                 h2_reg.hsync_end = addrdatain[12:0];
                 prog_st_d = wr_req;
-            end else if (addrdatain_d == 38) begin
+            end else if (addrdatain_d == 32'h00000038) begin
                 v1_reg.vend = addrdatain[12:0];
                 v1_reg.vsize = addrdatain[25:13];
                 prog_st_d = wr_req;
-            end else if (addrdatain_d == 40) begin
+            end else if (addrdatain_d == 32'h00000040) begin
                 v2_reg.vsync_start = addrdatain[25:13];
                 v2_reg.vsync_end = addrdatain[12:0];
                 prog_st_d = wr_req;
-            end else if (addrdatain_d == 48) begin
+            end else if (addrdatain_d == 32'h00000048) begin
                 base_address = addrdatain;
                 prog_st_d = wr_req;
-            end else if (addrdatain_d == 50) begin
+            end else if (addrdatain_d == 32'h00000050) begin
                 lineinc = addrdatain;
                 prog_st_d = wr_req;
             end else begin
